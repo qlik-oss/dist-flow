68c202a7217fdbd9a803f7f7e6aab028ee65b771
Tobias Linsefors <tobias.linsefors@qlik.com>
2021-05-31 13:35:58 +0200

    remove stuff

--- tools/qext.js
+++
@@ -1,6 +0,0 @@
-/* eslint import/no-amd: 0 */
-/* eslint prefer-arrow-callback: 0 */
-/* global define */
-define(['{{DIST}}'], function m(supernova) {
-  return supernova;
-});
--- tools/update-version.js
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env node
-
-const path = require('path');
-
-const pkg = require(path.resolve(process.cwd(), 'package.json')); // eslint-disable-line
-
-// NOT SURE WHAT TO DO HERE YET
