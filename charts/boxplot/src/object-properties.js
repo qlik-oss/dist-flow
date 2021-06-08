a8f7c0a1ae85dd849b846bb86650633d67c2d46b
Tobias Linsefors <tobias.linsefors@qlik.com>
2021-06-08 15:32:53 +0200

    docs: improve boxplot object-properties

--- charts/boxplot/src/object-properties.js
+++ charts/boxplot/src/object-properties.js
@@ -1,14 +1,164 @@
+/**
+ * @namespace properties
+ * @entry
+ */
 const properties = {
-  qHyperCubeDef: {
-    qDimensions: [],
-    qMeasures: [],
-    qInitialDataFetch: [{ qWidth: 10, qHeight: 500 }],
-    qSuppressZero: false,
-    qSuppressMissing: true,
+  /**
+   * Current version of this generic object definition.
+   * @type {string}
+   * @default
+   */
+  // TODO: setup version
+  // version: process.env.PACKAGE_VERSION,
+
+  /**
+   * Settings specific to the boxplot
+   * @type {object}
+   */
+  boxplotDef: {
+    /**
+     * Extends `HyperCubeDef`, see Engine API: `HyperCubeDef`.
+     * @extends {HyperCubeDef}
+     */
+    qHyperCubeDef: {
+      qDimensions: [],
+      qMeasures: [],
+      qInitialDataFetch: [{ qWidth: 10, qHeight: 500 }],
+      qSuppressZero: false,
+      qSuppressMissing: true,
+    },
+    /**
+     * Box plot calculation settings.
+     * @type {object}
+     */
+    calculations: {
+      /**
+       * Use automatic calculations
+       * @type {boolean}
+       * @default
+       */
+      auto: true,
+      /**
+       * Auto calculation modes
+       * @type {'tukey'|'fractiles'|'stdDev'}
+       * @default
+       */
+      mode: 'tukey',
+      /**
+       * Box plot calculation settings
+       * @type {object}
+       */
+      parameters: {
+        /**
+         * Number of interquartile ranges for whiskers.
+         * @type {number}
+         * @default
+         */
+        tukey: 1.5,
+        /**
+         * A number representing the two whisker fractiles as N and 1-N
+         * @type {number}
+         * @default
+         */
+        fractiles: 0.01,
+        /**
+         * Standard deviation spread for whiskers
+         * @type {number}
+         * @default
+         */
+        stdDev: 3,
+      },
+    },
+    /**
+     * @type {object}
+     */
+    color: {
+      // TODO:
+    },
+    /**
+     * Box plot elements settings.
+     * @type {object}
+     */
+    elements: {
+      /**
+       * Box plot element settings
+       * @type {object}
+       */
+      firstWhisker: {
+        /**
+         * Label for the boxplot element
+         * @type {Qlik.Engine.StringExpressionContainer}
+         * @default
+         */
+        name: '',
+        /**
+         * Expression for the boxplot element
+         * @type {Qlik.Engine.ValueExpressionContainer}
+         */
+        expression: null,
+      },
+      boxStart: {
+        name: '',
+        expression: null,
+      },
+      boxMiddle: {
+        name: '',
+        expression: null,
+      },
+      boxEnd: {
+        name: '',
+        expression: null,
+      },
+      lastWhisker: {
+        name: '',
+        expression: null,
+      },
+      /**
+       * Box plot outliers element
+       * @type {object}
+       */
+      outliers: {
+        /**
+         * Show the outliers.
+         * @type {boolean}
+         */
+        include: true,
+        /**
+         * @type {boolean}
+         */
+        sortOutliers: true,
+      },
+    },
+    /**
+     * @type {object}
+     */
+    presentation: {
+      // TODO:
+    },
   },
+  /**
+   * Show title for the visualization.
+   * @type {boolean=}
+   * @default
+   */
   showTitles: true,
-  title: '',
+  /**
+   * Visualization subtitle.
+   * @type {(string|StringExpression)=}
+   * @default
+   */
   subtitle: '',
+  /**
+   * Visualization title.
+   * @type {(string|StringExpression)=}
+   * @default
+   */
+  title: '',
+  /**
+   * Visualization footnote.
+   * @type {(string|StringExpression)=}
+   * @default
+   */
   footnote: '',
 };
 
