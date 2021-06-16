dcc013342c94b527f0bd29a924fc02f0af53b02e
Tobias Linsefors <tobias.linsefors@qlik.com>
2021-06-16 15:06:03 +0200

    remove dependency on qlik-modifiers (color by measure is not supported)

--- chart-modules/common/extra/property-logic-helper.js
+++ chart-modules/common/extra/property-logic-helper.js
@@ -1,18 +1,8 @@
-import Modifiers from 'qlik-modifiers';
-
-function getExpression(measure, colorByActiveMeasure) {
-  const hasActiveModifiers = Modifiers.hasActiveModifiers({ measures: [measure] });
-  // Only when a modifier is applied to a master measure and color by 'Measure 1' (or 'Measure 2', etc.)
-  return hasActiveModifiers && colorByActiveMeasure ? measure.qDef.qDef : undefined;
-}
-
 function getColorByDefinition(columnType, isAttribute, target, colorByActiveMeasure) {
   const ret = {};
   let qDef;
   let hasLabelExpression;
   let hasLabel;
-  let qLibraryId;
-  let expression;
 
   switch (columnType) {
     case 'dimension':
@@ -38,38 +28,6 @@ function getColorByDefinition(columnType, isAttribute, target, colorByActiveMeas
             ret.label = qDef.qFieldLabels[0];
           } else {
             ret.label = qDef.qFieldDefs[0];
-          }
-        }
-      }
-      break;
-    case 'measure':
-      qLibraryId = isAttribute ? target.qLibraryId : Modifiers.measureBase.getLibraryId(target);
-      if (qLibraryId) {
-        ret.type = 'measure';
-        ret.name = qLibraryId;
-        ret.id = qLibraryId;
-        expression = getExpression(target, colorByActiveMeasure);
-        if (expression) {
-          ret.expression = expression;
-        }
-      } else {
-        ret.type = 'expression';
-        if (isAttribute) {
-          ret.name = target.label || target.qExpression;
-          ret.id = target.qExpression;
-        } else {
-          qDef = target.qDef;
-          hasLabelExpression = !!qDef.qLabelExpression;
-          hasLabel = !!qDef.qLabel;
-
-          ret.name = qDef.qDef;
-
-          if (hasLabelExpression) {
-            ret.label = qDef.qLabelExpression;
-          } else if (hasLabel) {
-            ret.label = qDef.qLabel;
-          } else {
-            ret.label = qDef.qDef;
           }
         }
       }
