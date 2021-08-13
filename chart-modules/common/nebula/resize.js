cbf1fc9be9c71663069135fb2a75f75b3e8504e2
Tobias Linsefors <T-Wizard@users.noreply.github.com>
2021-08-13 11:27:00 +0200

    fix: enable/disable selection on start/end edit sheet (#8)

--- chart-modules/common/nebula/resize.js
+++ chart-modules/common/nebula/resize.js
@@ -1,13 +1,15 @@
-import { useOptions, usePromise, useRect } from '@nebula.js/stardust';
+import { useConstraints, useOptions, usePromise, useRect } from '@nebula.js/stardust';
 
 export default function useResize(chartInstance) {
   const rect = useRect();
   const options = useOptions();
+  const constraints = useConstraints();
 
   const [, error] = usePromise(async () => {
     if (!chartInstance) {
       return;
     }
+    chartInstance.updateConstraints(constraints);
     chartInstance.options = options;
     await chartInstance.resize();
   }, [rect.width, rect.height]);
