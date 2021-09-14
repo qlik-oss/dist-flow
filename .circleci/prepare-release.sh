ab2817ecd930eb511aa560fc63016bb10928da06
Tobias Linsefors <tobias.linsefors@qlik.com>
2021-09-14 16:03:01 +0200

    chore: support main branch (not master)

--- .circleci/prepare-release.sh
+++ .circleci/prepare-release.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-set -eov pipefail
+set -eo pipefail
 
 TARGET_FILE="ENV.tmp"
 
-TAG=$(git tag -l --points-at HEAD)
-BRANCH=$(git branch -r --contains "$TAG" | grep -e master -e release | head -n 2 | tail -n -1 | sed -e "s: *origin/::")
+BRANCH=$(git branch -r --contains "$CIRCLE_TAG" | grep -e main -e master -e release | head -n 2 | tail -n -1 | sed -e "s: *origin/::")
 
-echo "export DIST_FLOW_TAG_TRIGGER=$TAG" >> $TARGET_FILE;
+echo "export DIST_FLOW_TAG_TRIGGER=$CIRCLE_TAG" >> $TARGET_FILE;
 echo "export DIST_FLOW_BRANCH=$BRANCH" >> $TARGET_FILE;
 
-if [[ "$TAG" == "trigger-prerelease" ]]; then
+if [[ "$CIRCLE_TAG" == "trigger-prerelease" ]]; then
   echo "Preparing prerelase";
   echo "export DIST_FLOW_RELEASE=prerelease" >> $TARGET_FILE;
-elif [[ "$TAG" == "trigger-release" ]]; then
+elif [[ "$CIRCLE_TAG" == "trigger-release" ]]; then
   echo "Preparing release";
   echo "export DIST_FLOW_RELEASE=release" >> $TARGET_FILE;
-elif [[ "$TAG" == "trigger-release-minor" ]]; then
+elif [[ "$CIRCLE_TAG" == "trigger-release-minor" ]]; then
   echo "Preparing forced minor release";
   echo "export DIST_FLOW_RELEASE=release" >> $TARGET_FILE;
 else
