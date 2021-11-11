871fedda2e65cb256e55531f962797953d8781e6
Christofer Bengtsson <cbt@qlik.com>
2021-11-11 12:28:33 +0100

    fix: histogram not updated after removing calculated condition (#117)

--- charts/histogram/src/histogram-properties-logic.js
+++ charts/histogram/src/histogram-properties-logic.js
@@ -1,6 +1,6 @@
 function onChangeCalcCond(data) {
   const baseCube = data?.qHyperCubeDef;
-  const generatedCube = data?.qUndoExclude?.qHyperCubeDef;
+  const generatedCube = data?.qUndoExclude?.box?.qHyperCubeDef;
   if (baseCube && generatedCube) {
     generatedCube.qCalcCond = baseCube.qCalcCond;
     generatedCube.qCalcCondition = baseCube.qCalcCondition;
