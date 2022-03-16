92755329b6ea682e50e16f95c6586d36ac261277
quanho <quan.ho@qlik.com>
2022-03-16 12:50:35 +0100

    fix: update object conversion for histogram

--- charts/histogram/src/ext.js
+++ charts/histogram/src/ext.js
@@ -2,6 +2,7 @@ import conversion from 'qlik-object-conversion';
 import pp from './histogram-properties';
 import exploreProperties from './histogram-explore-properties';
 import histogramExport from './histogram-export';
+import data from './histogram-data';
 
 export default function ext(env) {
   if (!env.anything?.sense) {
@@ -19,8 +20,25 @@ export default function ext(env) {
       sharing: true,
       viewData: true,
     },
-    importProperties: conversion.hypercube.importProperties,
-    exportProperties: conversion.hypercube.exportProperties,
+    importProperties: (exportFormat, initialProperties, extension, hypercubePath) => {
+      const dataDefinition = data(env);
+      const layoutService = { layout: { templateId: initialProperties.templateId } };
+      extension.mapProperties(layoutService);
+      const defaultPropertyValues = {
+        defaultDimension: extension.getDefaultDimensionProperties(),
+        defaultMeasure: extension.getDefaultMeasureProperties(),
+      };
+      return conversion.hypercube.importProperties({
+        exportFormat,
+        initialProperties,
+        dataDefinition,
+        defaultPropertyValues,
+        hypercubePath,
+      });
+    },
+    exportProperties(propertyTree, hyperCubePath, viewDataMode) {
+      return conversion.hypercube.exportProperties({ propertyTree, hyperCubePath, viewDataMode });
+    },
     requireNumericDimension: true,
     exportTableProperties(propertyTree) {
       // the hypercube that will be used for "building" the table is under `qUndoExclude.box.qHyperCubeDef`
