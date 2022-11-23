b1eb5625d65b292ed338d0f690c91903843628ac
Chinmay Ghungurde <chinmay.ghungurde@qlik.com>
2022-11-23 18:19:41 +0530

    fix: lint warning

--- charts/distributionplot/src/styling-panel-definition.js
+++ charts/distributionplot/src/styling-panel-definition.js
@@ -1,13 +1,11 @@
-const getStylingPanelDefinition = (bkgOptionsEnabled) => {
-  return {
-    component: 'styling-panel',
-    chartTitle: 'Object.DistributionPlot',
-    translation: 'LayerStyleEditor.component.styling',
-    subtitle: 'LayerStyleEditor.component.styling',
-    ref: 'components',
-    useGeneral: true,
-    useBackground: bkgOptionsEnabled,
-  };
-};
+const getStylingPanelDefinition = (bkgOptionsEnabled) => ({
+  component: 'styling-panel',
+  chartTitle: 'Object.DistributionPlot',
+  translation: 'LayerStyleEditor.component.styling',
+  subtitle: 'LayerStyleEditor.component.styling',
+  ref: 'components',
+  useGeneral: true,
+  useBackground: bkgOptionsEnabled,
+});
 
 export default getStylingPanelDefinition;
