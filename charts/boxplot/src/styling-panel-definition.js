54f64837e1719264b423a53de0fd0cbe6256a067
rajani.varpe <rajani.varpe@qlik.com>
2022-11-23 17:27:11 +0530

    feat(box-plot): removed eslint errors

--- charts/boxplot/src/styling-panel-definition.js
+++ charts/boxplot/src/styling-panel-definition.js
@@ -1,10 +1,10 @@
 export const getStylingPanelDefinition = (bkgOptionsEnabled) => {
   return {
-    component: 'styling-panel',
-    chartTitle: 'Object.BoxPlot',
-    translation: 'LayerStyleEditor.component.styling',
-    subtitle: 'LayerStyleEditor.component.styling',
-    ref: 'components',
+    component: "styling-panel",
+    chartTitle: "Object.BoxPlot",
+    translation: "LayerStyleEditor.component.styling",
+    subtitle: "LayerStyleEditor.component.styling",
+    ref: "components",
     useGeneral: true,
     useBackground: bkgOptionsEnabled,
   };
