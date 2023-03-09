914e64230c3ace2a75601cd433d67138b51649c6
Mahesh Chandra <mahesh.chandra@qlik.com>
2023-03-09 10:58:06 +0100

    feat: added styling for distplot

--- charts/distributionplot/src/distributionplot-view.js
+++ charts/distributionplot/src/distributionplot-view.js
@@ -12,6 +12,7 @@ import stringUtil from '@qlik/common/extra/string-util';
 import chartStyleUtils from '@qlik/common/extra/chart-style-utils';
 import hypercubeUtil from '@qlik/common/extra/hypercube-util';
 
+import { getAxisLabelSettings, getAxisTitleSettings } from '@qlik/common/extra/chart-style-component';
 import Jitter from './jitter';
 import columnOrderAdapter from './distributionplot-column-order-adapter';
 import CONSTANTS from './distributionplot-constants';
@@ -692,6 +693,8 @@ const DistributionPlot = ChartView.extend('DistributionPlot', {
 
       // ref-lines
       refLines: layout.refLine && layout.refLine.refLines,
+      ...getAxisTitleSettings(CONSTANTS.CHART_ID, theme, layout),
+      ...getAxisLabelSettings(CONSTANTS.CHART_ID, theme, layout),
     });
 
     // Box marker
