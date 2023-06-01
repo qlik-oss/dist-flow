19cd674245de3d14c93e702030bb33939afcff4f
Per Nylander <per.nylander@qlik.com>
2023-06-01 11:18:16 +0200

    test: update waterfall styling rendering

--- test/rendering/suites/waterfallchart/waterfallchart.spec.js
+++ test/rendering/suites/waterfallchart/waterfallchart.spec.js
@@ -6,6 +6,11 @@ TestGenerator.fromFixtures({
   serveConfig: {
     type: 'waterfallchart',
     entry: path.resolve(__dirname, '../../../../charts/waterfallchart/dist/sn-waterfall.js'),
+    flags: {
+      SENSECLIENT_IM_2020_STYLINGPANEL_WATERFALLCHART: true,
+      SENSECLIENT_IM_2020_WATERFALLCHART_BG: true,
+      CLIENT_IM_3364: true,
+    },
     port: 8017,
   },
   fixturePath: path.join(__dirname, '__fixtures__'),
